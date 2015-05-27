ef60b037fd99cdd3bee92643f6916d144ace5e07
Logan Short <lshort@stanford.edu>
2015-05-27 06:16:34 -0700

    Added KNN for tfidf

--- tfidf_parse.py
+++ tfidf_parse.py
@@ -1,6 +1,7 @@
 import buildwd
 import numpy as np
 from sklearn import linear_model
+from sklearn import neighbors
 
 def tfidf(mat=None, rownames=None):
     """TF-IDF on mat. rownames is unused; it's an argument only 
@@ -53,7 +54,41 @@ def tfidf_logreg(train_file):
     logreg.fit(trainMat[0:(trainMat.shape[0]*0.7),:], trainVals[0:(trainMat.shape[0]*0.7)])
     return logreg.score(trainMat[(trainMat.shape[0]*0.7):,:], trainVals[(trainMat.shape[0]*0.7):])
 
-logregscore = tfidf_logreg("training.txt")
+def tfidf_knn(train_file):
+    wd = buildwd.buildWD(train_file)
+    colnames = wd[1]
+    rownames = wd[2]
+    subjects = wd[3]
+    idf = tfidf(wd[0], rownames)
+    
+    trainMat = np.zeros((len(colnames), wd[0].shape[1]))
+    f = open(train_file)
+    matCol = 0
+    for line in f:
+        words = line.split()
+        if words[0] in colnames:
+            trainRow = np.zeros(wd[0].shape[1])
+            numWords = 0
+            for word in words[2:]:
+                pword = buildwd.processWord(word)
+                if pword in rownames:
+                    numWords += 1
+                    trainRow = trainRow + idf[0][rownames.index(pword)]
+            trainRow = (trainRow*1.0) / numWords
+            trainMat[matCol,:] = trainRow
+            matCol += 1
+    f.close()
+
+    trainVals = np.zeros(len(subjects))
+    for s in enumerate(subjects):
+        if s[1] == 'Sports':
+            trainVals[s[0]] = 1
+            
+    knn = neighbors.KNeighborsClassifier()
+    knn.fit(trainMat[0:(trainMat.shape[0]*0.7),:], trainVals[0:(trainMat.shape[0]*0.7)])
+    return knn.score(trainMat[(trainMat.shape[0]*0.7):,:], trainVals[(trainMat.shape[0]*0.7):])
+
+score = tfidf_knn("training.txt")
 
             
 
